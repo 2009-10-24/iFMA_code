2c1e07f0f01153dbd7cda5d3ca1a6f99fcb555bb
2009-10-24 <609236956@qq.com>
2015-12-10 01:24:38 +0800

    4444444444444

---
+++ link_java/src/com/soft/ware/link.java
@@ -0,0 +1,18 @@
+package com.soft.ware;
+/**
+ * 注释信息
+ * */
+public class link {
+
+	public static void main(String[] args) {
+		System.out.println("nihao");
+		System.out.println();
+
+		System.out.println("nihao");
+
+	        System.out.println("你好");
+		
+
+	}
+
+}
